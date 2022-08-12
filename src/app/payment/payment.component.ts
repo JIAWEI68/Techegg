e122d184710b0332dc5864d57ecb11421cd744ac
JIAWEI68 <83011056+JIAWEI68@users.noreply.github.com>
2022-08-12 17:11:46 +0800

    paypal services working properly

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -11,12 +11,13 @@ declare let paypal: any;
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css'],
 })
-export class PaymentComponent implements AfterViewChecked, OnInit {
+export class PaymentComponent implements OnInit {
   public paypalsConfig?: IPayPalConfig;
   sum: number = 0;
   count: number = 0;
   items!: items;
   addScript: boolean = false;
+  paymentList: payment[] = this.paymentService.getPayments();
   finalSum: number = this.paymentService.getTotalCost(this.sum);
   paypalConfig = {
     env: 'sandbox',
@@ -53,33 +54,33 @@ export class PaymentComponent implements AfterViewChecked, OnInit {
     },
   };
   constructor(private paymentService: PaymentService) {}
-  ngAfterViewChecked(): void {
-    if (!this.addScript) {
-      this.addPayPalScript().then(() => {
-        paypal.Button.render(this.paypalConfig, '#myPaypalBtns');
-      });
-    }
-  }
-  addPayPalScript() {
-    this.addScript = true;
-    return new Promise((resolve, reject) => {
-      let scriptTagElement = document.createElement('script');
-      scriptTagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
-      scriptTagElement.onload = resolve;
-      document.body.appendChild(scriptTagElement);
-    });
-  }
+  // ngAfterViewChecked(): void {
+  //   if (!this.addScript) {
+  //     this.addPayPalScript().then(() => {
+  //       paypal.Button.render(this.paypalConfig, '#myPaypalBtns');
+  //     });
+  //   }
+  // }
+  // addPayPalScript() {
+  //   this.addScript = true;
+  //   return new Promise((resolve, reject) => {
+  //     let scriptTagElement = document.createElement('script');
+  //     scriptTagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+  //     scriptTagElement.onload = resolve;
+  //     document.body.appendChild(scriptTagElement);
+  //   });
+  // }
 
   ngOnInit(): void {
-    this.paymentList = this.paymentList;
+    this.paymentList = this.paymentService.getPayments();
+    this.initiConfig();
   }
   delete(items: items) {
     this.paymentService.delete(items);
   }
-  paymentList = this.paymentService.getPayments();
   private initiConfig(): void {
     this.paypalsConfig = {
-      currency: 'SGD',
+      currency: 'USD',
       clientId: 'sb',
       createOrderOnClient: (data) =>
         <ICreateOrderRequest>{
@@ -87,28 +88,59 @@ export class PaymentComponent implements AfterViewChecked, OnInit {
           purchase_units: [
             {
               amount: {
-                currency_code: 'SGD',
-                value: this.finalSum.toFixed(2),
+                currency_code: 'USD',
+                value: this.finalSum.toString(),
                 breakdown: {
                   item_total: {
-                    currency_code: 'SGD',
-                    value: this.finalSum.toFixed(2),
+                    currency_code: 'USD',
+                    value: this.finalSum.toString()
                   },
                 },
               },
               items: this.paymentList.map((item) => {
                 return {
-                  name: item.name,
-                  quantity : this.paymentList.length.toFixed(2),
+                  name: "items",
+                  quantity : '1',
                   unit_amount: {
-                    currency_code: 'SGD',
-                    value: item.cost.toFixed(2),
+                    currency_code: 'USD',
+                    value: item.cost.toString()
                   },
                 };
               }),
             },
-          ],
+          ]
         },
+        advanced : {
+          commit : 'true'
+        },
+        style: {
+          label: 'paypal',
+          layout: 'vertical',
+          size: 'responsive',
+        },
+        onApprove : (data, actions) => {
+          console.log('onApprove - transaction was approved, but not authorized', data, actions);
+          actions.order.get().then( (details: any) => {
+            console.log('onApprove - you can get full order details inside onApprove: ', details);
+            this.resetStatus();
+          });
+        },
+        onClientAuthorization : (data) => {
+          console.log('onClientAuthorization - you should probably do something with the client authorization data', data);
+        },
+        onCancel(data, actions) {
+          console.log('OnCancel', data, actions);
+        },
+        onError : err => {
+          console.log('OnError', err);
+        },
+        onClick : (data, actions) => {
+          console.log('onClick', data, actions);
+        }
     };
+  }
+  resetStatus() {
+    paymentsList.splice(0, paymentsList.length);
+    this.finalSum = 0 ;
   }
 }
